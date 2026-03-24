56bdec751475e08430dba8f0457140507cd893fc
tbrown034 <tbrown034@gmail.com>
2026-03-24 18:28:16 -0400

    Add .gitignore for dist/, keep build output out of repo

--- dist/assets/selectAll-DmLYiGMk.js
+++
@@ -1,1 +0,0 @@
-import{S as t,X as n,r as o}from"./transform-DGvXF_4U.js";function a(e){return typeof e=="string"?new t([document.querySelectorAll(e)],[document.documentElement]):new t([n(e)],o)}export{a as s};
